8b1e947e89c51bd90fd06ca1cace1f792530bf8b
Porom <porom.kamal@gmail.com>
2023-03-20 23:16:58 -0400

    Rename DemoApplication to Application

--- backend/SpringServer/src/main/java/com/auth0/example/Application.java
+++ backend/SpringServer/src/main/java/com/auth0/example/Application.java
@@ -1,15 +1,14 @@
 package com.auth0.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 @ComponentScan
-public class DemoApplication {
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
