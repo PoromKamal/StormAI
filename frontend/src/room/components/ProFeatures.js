2666731ed8407b763597b1c538784e55606289e8
Mir Safwat Ayser <aysermir@gmail.com>
2023-03-31 02:29:48 -0400

    Modified payment landing page

--- frontend/src/room/components/ProFeatures.js
+++ frontend/src/room/components/ProFeatures.js
@@ -26,11 +26,11 @@ const ProFeatures = () => {
       bg-gradient-to-r from-blue-900 via-indigo-500 to-cyan-400 bg-clip-text text-transparent">StormAI PRO</h1>
       <p className="pro-features-description">Upgrade to the pro version of Storm AI to enjoy the following features:</p>
       <ul className="pro-features-list">
-        <li className="pro-feature">Feature 1</li>
-        <li className="pro-feature">Feature 2</li>
-        <li className="pro-feature">Feature 3</li>
-        <li className="pro-feature">Feature 4</li>
-        <li className="pro-feature">Feature 5</li>
+        <li className="pro-feature">Artist Bot</li>
+        <li className="pro-feature">Story Bot</li>
+        <li className="pro-feature">Allow 5+ users on the board at a time!</li>
+        <li className="pro-feature">Save unlimited whiteboards to your account</li>
+        <li className="pro-feature">Upcoming: ChatBot</li>
       </ul>
       <p className="pro-features-upgrade">Don't miss out on these amazing features. Upgrade now!</p>
       <p className="proButton"><PayButton /></p>
